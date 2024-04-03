255e9955d01eba34bf39ed0e10fdbfabb5f76cde
Moataz Ghazy <m.ghazy44@hotmail.com>
2024-04-03 10:13:12 -0400

    created initial files

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+from json import load
+from face_recognition as load_image_file, face_encodings
+import math
 
-cap.release()
-cv2.destroyAllWindows()
-# test
+known_face_encodings = []
+known_face_names = []
+
+f = open("data-1.json")
+data = load(f)
+for e in data:
+  enc = face_encodings(load_image_file(f"images/{e['img']}"))
+  known_face_encodings.append(enc[0])
+  known_face_names.append(e["name"])
+f.close()
+
+face_locations = []
+face_encodings = []
+face_names = []
